6aae89ca1ed98dc7f161dc0c7b65f59d0e26ebbf
madaravag08 <MV1611@s1vsk.lv>
2026-04-19 00:14:35 +0300

    Add files via upload

---
+++ akt2.js
@@ -0,0 +1,10 @@
+//IZVADA ŠODIENAS DATUMU
+
+function getFormattedDate() {
+    const today = new Date();
+    const day = String(today.getDate()).padStart(2, '0');
+    const month = String(today.getMonth() + 1).padStart(2, '0'); // Months are 0-based
+    const year = today.getFullYear();
+    return `${day}/${month}/${year}`;
+    }
+    document.getElementById('datums').textContent = getFormattedDate()
